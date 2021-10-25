eb5fe327842f43613e0579f81b3f2909d6377645
dmitriyabramovskyi <d.abramovskyi@gmail.com>
2021-10-25 20:41:45 +0200

    First commit

---
+++ src/main/java/com/dmytriy_abramovskyi/da_crypt/DaCryptApplication.java
@@ -0,0 +1,13 @@
+package com.dmytriy_abramovskyi.da_crypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaCryptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaCryptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dmytriy_abramovskyi/da_crypt/DaCryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmytriy_abramovskyi.da_crypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaCryptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
